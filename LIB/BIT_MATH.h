3fd4486bf6512c44d59ffa52fa0f604bc4487566
salaheldeenyasser <salaheldeenyasser@gmail.com>
2026-03-29 01:27:25 +0200

    Enhance BIT_MATH.h and STD_TYPES.h with additional macros and type definitions for improved functionality and clarity

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -1,15 +1,53 @@
-/***************************************************************************************/
-/****************************  IMT School Training Center ******************************/
-/***************************************************************************************/
-/** This file is developed by IMT School training center, All copyrights are reserved **/
-/***************************************************************************************/
 #ifndef _BIT_MATH_H
 #define _BIT_MATH_H
 
+// Bit manipulation macros for AVR microcontroller programming
 #define SET_BIT(VAR,BITNO) (VAR) |=  (1 << (BITNO))
 #define CLR_BIT(VAR,BITNO) (VAR) &= ~(1 << (BITNO))
 #define TOG_BIT(VAR,BITNO) (VAR) ^=  (1 << (BITNO))
 #define GET_BIT(VAR,BITNO) (((VAR) >> (BITNO)) & 0x01)
+#define ASSIGN_BIT(VAR,BITNO,VAL) (VAR) = ((VAR) & ~(1 << (BITNO))) | ((VAL) << (BITNO))
+
+// NIBBLE Operations
+#define SET_NIBBLE(VAR) (VAR) |= 0x0F
+#define CLR_NIBBLE(VAR) (VAR) &= 0xF0
+#define TOG_NIBBLE(VAR) (VAR) ^= 0x0F
+#define GET_NIBBLE(VAR) ((VAR) & 0x0F)
+#define ASSIGN_NIBBLE(VAR,VAL) (VAR) = ((VAR) & 0xF0) | ((VAL) & 0x0F)
+
+// Byte Operations
+#define SET_BYTE(VAR) (VAR) |= 0xFF
+#define CLR_BYTE(VAR) (VAR) &= 0x00
+#define TOG_BYTE(VAR) (VAR) ^= 0xFF
+#define GET_BYTE(VAR) ((VAR) & 0xFF)
+#define ASSIGN_BYTE(VAR,VAL) (VAR) = (VAL)
+
+// Word Operations (for 16-bit registers)
+#define SET_WORD(VAR) (VAR) |= 0xFFFF
+#define CLR_WORD(VAR) (VAR) &= 0x0000
+#define TOG_WORD(VAR) (VAR) ^= 0xFFFF
+#define GET_WORD(VAR) ((VAR) & 0xFFFF)
+#define ASSIGN_WORD(VAR,VAL) (VAR) = (VAL)
+
+// Additional utility macros
+#define BIT_MASK(BITNO) (1 << (BITNO))
+#define NIBBLE_MASK(NIBBLENO) (0x0F << ((NIBBLENO) * 4))
+#define BYTE_MASK(BYTENO) (0xFF << ((BYTENO) * 8))
+#define WORD_MASK(WORDNO) (0xFFFF << ((WORDNO) * 16))
+
+/* Example usage:
+ To set bit 3 of PORTA: SET_BIT(PORTA, 3);
+ To clear bit 2 of PORTB: CLR_BIT(PORTB, 2);
+ To toggle bit 0 of PORTC: TOG_BIT(PORTC, 0);
+ To get the value of bit 1 of PORTD: GET_BIT(PORTD, 1);
+ To assign value 1 to bit 4 of PORTE: ASSIGN_BIT(PORTE, 4, 1);
+ To set the lower nibble of PORTF: SET_NIBBLE(PORTF);
+ To clear the upper nibble of PORTG: CLR_NIBBLE(PORTG);
+ To toggle the entire byte of PORTH: TOG_BYTE(PORTH);
+ To get the value of the entire byte of PORTJ: GET_BYTE(PORTJ);
+ To assign value 0xAB to the entire byte of PORTK: ASSIGN_BYTE(PORTK, 0xAB);
+ Note: These macros are designed for use with AVR microcontroller registers and may need to be adapted for other platforms or data types.
+*/
 
 
 #endif
--- LIB/STD_TYPES.h
+++ LIB/STD_TYPES.h
@@ -1,21 +1,127 @@
-/***************************************************************************************/
-/****************************  IMT School Training Center ******************************/
-/***************************************************************************************/
-/** This file is developed by IMT School training center, All copyrights are reserved **/
-/***************************************************************************************/
 #ifndef _STD_TYPES_H
 #define _STD_TYPES_H
 
 typedef unsigned char u8;
 typedef unsigned short int u16;
 typedef unsigned long  int u32;
+typedef unsigned long long int u64;
 
 typedef signed char s8;
 typedef signed short int s16;
 typedef signed long  int s32;
+typedef signed long long int s64;
 
 typedef float  f32;
 typedef double f64;
+
+typedef enum
+{
+    FALSE = 0,
+    TRUE = 1
+} bool;
+
+typedef enum
+{
+    LOW = 0,
+    HIGH = 1
+} logic_level;
+
+typedef enum
+{
+    OFF = 0,
+    ON = 1
+} state;
+
+typedef enum
+{
+    DISABLED = 0,
+    ENABLED = 1
+} status;
+
+typedef enum
+{
+    ERROR = 0,
+    SUCCESS = 1
+} error_status;
+
+typedef enum
+{
+    IDLE = 0,
+    BUSY = 1
+} operation_status;
+
+typedef enum
+{
+    INPUT = 0,
+    OUTPUT = 1
+} pin_direction;
+
+typedef enum
+{
+    LOW_NIBBLE = 0,
+    HIGH_NIBBLE = 1
+} nibble_position;
+
+typedef enum
+{
+    FIRST_BYTE = 0,
+    SECOND_BYTE = 1
+} byte_position;
+
+typedef enum
+{
+    FIRST_WORD = 0,
+    SECOND_WORD = 1
+} word_position;
+
+typedef enum
+{
+    NO_ERROR = 0,
+    NULL_POINTER_ERROR = 1,
+    OUT_OF_RANGE_ERROR = 2,
+    BUSY_ERROR = 3,
+    UNKNOWN_ERROR = 4
+} error_code;
+
+typedef enum
+{
+    LOW_PRIORITY = 0,
+    MEDIUM_PRIORITY = 1,
+    HIGH_PRIORITY = 2,
+    CRITICAL_PRIORITY = 3
+} priority_level;
+
+typedef enum
+{
+    MODE_0 = 0,
+    MODE_1 = 1,
+    MODE_2 = 2,
+    MODE_3 = 3
+} operation_mode;
+
+typedef enum
+{
+    IDLE_STATE = 0,
+    ACTIVE_STATE = 1,
+    SLEEP_STATE = 2,
+    DEEP_SLEEP_STATE = 3
+} power_state;
+
+typedef enum
+{
+    NO_INTERRUPT = 0,
+    INTERRUPT_0 = 1,
+    INTERRUPT_1 = 2,
+    INTERRUPT_2 = 3
+} interrupt_source;
+
+typedef enum
+{
+    LOW_SPEED = 0,
+    MEDIUM_SPEED = 1,
+    HIGH_SPEED = 2,
+    MAX_SPEED = 3
+} speed_setting;
 
 #define NULL         0
 #define OK           0
